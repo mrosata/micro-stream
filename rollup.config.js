ae44b1e03a51b38ab6ec28a169f491eac46fb0bc
Michael Rosata <mrosata1984@gmail.com>
2016-11-22 23:07:57 -0500

    fixed transpile problem

--- rollup.config.js
+++ rollup.config.js
@@ -18,7 +18,7 @@ export default {
     {
       dest: pkg['main'],
       format: 'umd',
-      moduleName: 'microStream',
+      moduleName: 'rollupStarterProject',
       sourceMap: true
     },
     {
